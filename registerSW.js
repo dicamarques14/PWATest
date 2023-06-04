62529f89f5c328282aedc904ba013f6545104235
Dicamarques <dcabrantesm@gmail.com>
2023-06-04 10:03:42 +0100

    b3

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: '/PWATest/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PWATest/sw.js', { scope: '/PWATest/' })})}
